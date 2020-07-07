a7eceb848d4f2fe0a36abed58458bbfff21e7d95
Dave <davew.gunawan@gmail.com>
2020-07-07 16:19:01 +0700

    adding main templates

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,9 +1,14 @@
 from flask import Blueprint,render_template
-
+from myproject.models import Product
 
 core = Blueprint('core',__name__,template_folder='templates/core')
 
 
 @core.route('/')
 def index():
-    return render_template("index.html")
+    products = Product.query.all()
+    return render_template("index.html", products=products)
+
+@core.route('/learn_more')
+def learn_more():
+    return 'Hah go back to the nether to kill piglins!'#render_template("index.html")
