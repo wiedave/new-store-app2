311df5dfebca6300024ff4085c2da0cbc391d927
Gunawan <wiegunawan@live.com>
2020-07-07 22:26:47 +0700

    Update Product edit dan delete

--- myproject/admin/products/forms.py
+++ myproject/admin/products/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SelectField,IntegerField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired,DataRequired
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flask_wtf.file import FileField,FileAllowed
 from myproject.models import Group,Brand
@@ -15,5 +15,10 @@ class ProductForm(FlaskForm):
     group = QuerySelectField('Group Name',query_factory=group_list,allow_blank=False,get_label='groupname')
     brand = QuerySelectField('Brand Name',query_factory=brand_list,allow_blank=False,get_label='brandname')
     name = StringField('Product Name',validators=[InputRequired()])
-    photo = FileField('Product Photo',validators=[InputRequired(),FileAllowed(['jpg','png'])])
+    photo = FileField('Product Photo',validators=[InputRequired(),FileAllowed(['jpg','png','jpeg'])])
+    price = IntegerField('Product Price',validators=[InputRequired()])
+
+class EditProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[InputRequired()])
+    photo = FileField('Product Photo',validators=[FileAllowed(['jpg','png','jpeg'])])
     price = IntegerField('Product Price',validators=[InputRequired()])
