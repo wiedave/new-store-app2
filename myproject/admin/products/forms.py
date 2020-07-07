54333ef03986ffc9580c83861ea7544abdea76a3
Dave <davew.gunawan@gmail.com>
2020-07-07 21:23:51 +0700

    added home page

--- myproject/admin/products/forms.py
+++ myproject/admin/products/forms.py
@@ -15,5 +15,5 @@ class ProductForm(FlaskForm):
     group = QuerySelectField('Group Name',query_factory=group_list,allow_blank=False,get_label='groupname')
     brand = QuerySelectField('Brand Name',query_factory=brand_list,allow_blank=False,get_label='brandname')
     name = StringField('Product Name',validators=[InputRequired()])
-    photo = FileField('Product Photo',validators=[InputRequired(),FileAllowed(['jpg','png'])])
+    photo = FileField('Product Photo',validators=[InputRequired(),FileAllowed(['jpg','png','jpeg'],message="File must be jpg, png or jpeg!")])
     price = IntegerField('Product Price',validators=[InputRequired()])
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -3,11 +3,24 @@ from myproject.models import Product
 
 core = Blueprint('core',__name__,template_folder='templates/core')
 
+def harga(price):
+    num = list(str(price))
+    if len(num) == 4 or len(num) == 5 or len(num) == 6:
+        num.insert(-3,'.')
+    elif len(num) == 7 or len(num) == 8 or len(num) == 9:
+        num.insert(-3,'.')
+        num.insert(-7,'.')
+    elif len(num) == 10 or len(num) == 11 or len(num) == 12:
+        num.insert(-3,'.')
+        num.insert(-7,'.')
+        num.insert(-11,'.')
+    num = "".join(num)
+    return num
 
 @core.route('/')
 def index():
     products = Product.query.all()
-    return render_template("index.html", products=products)
+    return render_template("index.html", products=products, harga=harga)
 
 @core.route('/learn_more')
 def learn_more():
