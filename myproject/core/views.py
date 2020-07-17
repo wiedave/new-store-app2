e14e2f9ed0596d4e3c715719172f5fa6d5ce2091
Dave <davew.gunawan@gmail.com>
2020-07-17 21:07:03 +0700

    a few final changes

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint,render_template, request, redirect, url_for
-from myproject.models import Product
+from flask import Blueprint,render_template, request, redirect, url_for, abort
+from myproject.models import Product, Group, Brand
 from myproject.core.forms import SearchForm
 
 core = Blueprint('core',__name__,template_folder='templates/core')
@@ -21,25 +21,67 @@ def harga(price):
 @core.route('/', methods=["GET", "POST"])
 def index():
     products = Product.query.all()
+    brands = Brand.query.all()
     form = SearchForm()
+    groups = Group.query.all()
     if form.validate_on_submit():
         return redirect(url_for('core.search', search=form.search.data))
 
-    return render_template("index.html", products=products, harga=harga, form=form)
+    return render_template("index.html", products=products, harga=harga, form=form, groups=groups, brands=brands)
 
 @core.route('/search/<search>', methods=["GET", "POST"])
 def search(search):
     form = SearchForm()
+    brands = Brand.query.all()
+    groups = Group.query.all()
     sea = str(search)
     product = Product.query.filter(Product.name.like('%'+sea+'%')).all()
     if form.validate_on_submit():
         return redirect(url_for('core.search', search=form.search.data))
 
 
-    return render_template('search.html', search=product, harga=harga, name=sea, form=form)
+    return render_template('search.html', search=product, harga=harga, name=sea, form=form, groups=groups, brands=brands)
 
-@core.route('/learn_more/<int:id>')
+@core.route('/learn_more/<int:id>', methods=["GET", "POST"])
 def learn_more(id):
+    form = SearchForm()
+    groups = Group.query.all()
+    brands = Brand.query.all()
     product = Product.query.filter(Product.id==id).first()
+    if not product:
+        abort(404)
 
-    return render_template("read_more.html", product=product)
+    if form.validate_on_submit():
+        return redirect(url_for('core.search', search=form.search.data))
+
+    return render_template("read_more.html", product=product, harga=harga, form=form, groups=groups, brands=brands)
+
+@core.route('/group/<name>', methods=["GET", "POST"])
+def groups(name):
+    form = SearchForm()
+    groups = Group.query.all()
+    brands = Brand.query.all()
+    products = Product.query.filter(Product.group_id==name).all()
+
+    if not products:
+        abort(404)
+
+    if form.validate_on_submit():
+        return redirect(url_for('core.search', search=form.search.data))
+
+    return render_template('groups.html', harga=harga, form=form, products=products, groups=groups, brands=brands)
+
+@core.route('/brand/<id>', methods=["GET", "POST"])
+def brands(id):
+    form = SearchForm()
+    brands = Brand.query.all()
+    groups = Group.query.all()
+    products = Product.query.filter(Product.brand_id==id).all()
+
+    if not products:
+        abort(404)
+
+    if form.validate_on_submit():
+        return redirect(url_for('core.search', search=form.search.data))
+
+    return render_template('groups.html', harga=harga, form=form, products=products, groups=groups, brands=brands)
